988c3fbb26083551fd8c9ae1e35c0cd6b4e3005d
m0satron <msebdani@gmail.com>
2024-12-09 12:10:42 +0100

    feat: add decision tree dialogue container

---
+++ src/components/DecisionTreeDialog.vue
@@ -0,0 +1,42 @@
+<template>
+  <div ref="tooltip" class="tooltip bg-color-blue color-white p-md rounded">
+    <img :src="imgSrc" :class="{ portrait: !thumbnail }" />
+    <hr class="border-color-primary mb-md" />
+
+    <div>
+      <strong class="font-weight-black font-size-body">{{ title }} </strong>
+      <p>{{ content }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  imgSrc: string
+  thumbnail: boolean
+  title: string
+  content: string
+}>()
+</script>
+
+<style scoped lang="scss">
+.tooltip {
+  position: fixed;
+  top: 40rem;
+  display: flex;
+  flex-direction: column;
+  z-index: 4;
+  left: 14rem;
+  width: 40rem;
+  img {
+    width: 3rem;
+    margin-bottom: 1rem;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .portrait {
+    width: 180px;
+    border-radius: 1rem;
+  }
+}
+</style>
